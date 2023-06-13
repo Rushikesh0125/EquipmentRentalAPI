ac092e38dfb940f17f8446c6d29b44aee911dcf9
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-06-13 11:46:02 +0530

    added entity attribute validations and implemented hourly rate function

--- src/main/java/com/example/EquipmentRental/Dto/BookingRequest.java
+++ src/main/java/com/example/EquipmentRental/Dto/BookingRequest.java
@@ -1,19 +1,39 @@
 package com.example.EquipmentRental.Dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
+
+/*
+*Data transfer object for requesting a booking
+* The request should have defined parameters
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookingRequest {
+
+    //Request should contain following parameters
+    @NotNull
     private Long equipmentId;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime startDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
     private LocalDateTime endDate;
+
 }
--- src/main/java/com/example/EquipmentRental/Service/BookingService.java
+++ src/main/java/com/example/EquipmentRental/Service/BookingService.java
@@ -2,11 +2,17 @@ package com.example.EquipmentRental.Service;
 
 import com.example.EquipmentRental.Dto.BookingRequest;
 import com.example.EquipmentRental.Model.Booking;
+import com.example.EquipmentRental.Model.Equipment;
 import com.example.EquipmentRental.Repository.BookingRepository;
+import com.example.EquipmentRental.Repository.EquipmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +21,39 @@ public class BookingService {
     @Autowired
     private final BookingRepository bookingRepository;
 
-    public void addBooking(@RequestBody BookingRequest bookingRequest){
+    @Autowired
+    private final EquipmentRepository equipmentRepository;
+
+    /*
+    @Param Takes a Request body DTO as param
+    @Returns total price for booking
+     */
+    public double getPricePerHour(@RequestBody BookingRequest bookingRequest) throws NoSuchElementException {
+        //Getting the equipment from equipment repository
+        Optional<Equipment> eqp = equipmentRepository.findById(bookingRequest.getEquipmentId());
+
+        //Calculating the duration between start and end date
+        Duration duration =  Duration.between(bookingRequest.getEndDate(), bookingRequest.getStartDate());
+
+        //returns total price as total price = duration in hours * price per hour
+        return Math.abs((eqp.orElseThrow().getPricePerHour())*duration.toHours());
+    }
+
+    /*
+    @param takes Booking request DTO
+    *constructs booking model and saves it into repository
+     */
+    public void addBooking(@RequestBody BookingRequest bookingRequest) throws Exception{
+        double totalAmount = getPricePerHour(bookingRequest);
+        if(bookingRequest.getStartDate().isAfter(bookingRequest.getEndDate())){
+            throw new Exception("Invalid Start or End date");
+        }
+
         Booking booking = Booking.builder()
                 .startDate(bookingRequest.getStartDate())
                 .endDate(bookingRequest.getEndDate())
                 .equipmentId(bookingRequest.getEquipmentId())
+                .totalAmount(totalAmount)
                 .build();
 
         bookingRepository.save(booking);
